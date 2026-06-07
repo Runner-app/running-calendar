215fb79ac5026d3a4c40ba7fe7028a0fba825ebf
DESKTOP-3634DSN\piotr <piotr.stronciwilk@gmail.com>
2026-06-07 11:28:07 +0200

    Migration to react

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import htmlInject from 'vite-plugin-html-inject';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    htmlInject()
-  ]
-});
+  plugins: [react()],
+})
