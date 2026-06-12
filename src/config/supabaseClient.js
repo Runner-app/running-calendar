a6d8e45f90f0583e58eb237c2d3f85bec331d6c4
DESKTOP-3634DSN\piotr <piotr.stronciwilk@gmail.com>
2026-06-12 14:55:36 +0200

    Minor changes - authentication

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pxwwtwcbkecjyaneedhv.supabase.co";
-const supabaseAnonKey = "sb_publishable_r3OlolMVFraZ_Zf4iawQFw_o2j9n2oZ";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
